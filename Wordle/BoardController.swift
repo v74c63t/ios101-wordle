8e06ea0c2b80f0d9abfda0337322a7c5db958cc2
Vanessa Tang <vanesst5@uci.edu>
2023-09-16 16:00:21 -0700

    Exercise 6

--- Wordle/BoardController.swift
+++ Wordle/BoardController.swift
@@ -65,6 +65,7 @@ class BoardController: NSObject,
     // Checkpoint: After finishing this exercise, you should now be able to tap on the delete keyboard cell and have the last letter deleted on the board! If it's not working, check your work on this exercise and make sure deleteLastCharacter() is called properly in exercise 3.
     // START YOUR CODE HERE
     // ...
+    cell.clearLetter()
     // END YOUR CODE HERE
   }
 }
